5769c525d0e22a7c1fff7b754fc42c56904af3a5
Bri <brianjcarroll0@gmail.com>
2025-04-10 13:12:53 -0400

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
   const introText = "Hello! These are a series of sketches currently in development for a show called Complimentary TV. They are written, directed, produced, and edited by Tom Hunt. If you enjoy them, please make sure to let us know! Thanks for stopping by! "
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-overpass)]">
+    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-overpass)] max-w-4xl m-auto">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-4xl sm:text-6xl font-bold text-center m-auto font-[family-name:var(--font-space-mono)]">
           Complimentary TV
@@ -21,7 +21,7 @@ export default function Home() {
         <p className="text-lg sm:text-xl text-center m-auto">
           <a className="text-blue-500 underline" href="mailto:tomhunt.us@gmail.com">tomhunt.us@gmail.com</a>
         </p>
-        <hr className="w-[100px] md:w-[200px] border-t-2 border-gray-300 m-auto" />
+        <hr className="w-[75px] md:w-[100px] border-t-2 border-gray-300 m-auto" />
         {videoEmbedUrls.map((url, index) => (
           <div className="relative pb-[56.25%] w-full h-0" key={index}>
           <iframe
@@ -39,7 +39,9 @@ export default function Home() {
         
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-       
+       <p className="text-center">
+            We're making a few more sketches for the pilot, shoot me an email if you're looking for anything in particular. That's right, it's been me, Tom, the whole time, making this website. You caught me. Now what? Are you going to read me my rights?
+       </p>
       </footer>
     </div>
   );
